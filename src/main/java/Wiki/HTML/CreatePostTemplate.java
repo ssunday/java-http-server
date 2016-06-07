fba5e9f39bd75dbe3f018495bc2e73dcd3df3eb7
Sarah Sunday <ssunday@8thlight.com>
2016-06-07 12:11:03 -0500

    Minor tweak

--- src/main/java/Wiki/HTML/CreatePostTemplate.java
+++ src/main/java/Wiki/HTML/CreatePostTemplate.java
@@ -12,7 +12,7 @@ public class CreatePostTemplate extends PageTemplate {
     protected String getBody(){
         String body = InputForms.formStart("/create-post");
         body += InputForms.getTitleForm("Your_Post_Title_Must_Use_Underscores");
-        body += InputForms.getPostContentForm("You can use Markdown and HTML");
+        body += InputForms.getPostContentForm("You can use Markdown and HTML. Your mileage may vary.");
         body += InputForms.getSubmitButton("Create Post");
         body += InputForms.formEnd();
         return body;
